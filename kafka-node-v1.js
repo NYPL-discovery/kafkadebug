d1bb49ec613a347e836163843fd7321ab9679f0a
Paul Beaudoin <contact@paulbeaudoin.com>
2016-12-09 12:47:19 -0500

    fixed inproper use of completely obscure offsets call options (now using time: -1 and time: -2 to fetch latest and earliest offests, respect)

--- kafka-node-v1.js
+++ kafka-node-v1.js
@@ -25,7 +25,7 @@ class KafkaConsumer extends KafkaClient {
 
   consume (topic, offset, limit) {
     var payload = { topic, partition: 0, offset }
-    this.consumer = new Consumer(this.client, [payload], {groupId: conf.group_id, autoCommit: false, fromOffset: true})
+    this.consumer = new Consumer(this.client, [payload], {autoCommit: false, fromOffset: true})
     var consumed = 0
     this.consumer.on('message', function (m) {
       consumed += 1
@@ -39,6 +39,30 @@ class KafkaConsumer extends KafkaClient {
       console.log('ERROR: ', err)
     })
   }
+
+  offsets (topic) {
+    var offset = new kafka.Offset(this.client)
+    // time: -1 fetches "latest" offset(s) whereas time: -2 fetches "earliest", so have to issue both calls and merge:
+    return Promise.all(
+      [-1, -2].map((time) => {
+        return new Promise((resolve, reject) => {
+          offset.fetch([{ topic, partition: 0, time, maxNum: 2 }], (err, data) => {
+            if (err) reject(err)
+            else {
+              resolve(data[topic][0].map((v) => parseInt(v)))
+            }
+          })
+        })
+      })
+    ).then((offsets) => {
+      offsets = [].concat.apply([], offsets)
+      console.log('got offsets: ', offsets)
+      return {
+        min: Math.min.apply(null, offsets),
+        max: Math.max.apply(null, offsets)
+      }
+    })
+  }
 }
 
 function usage () {
@@ -49,8 +73,20 @@ function usage () {
 var topic = process.argv[2]
 
 if (!topic) common.error(usage())
+topic
 
 var offset = parseInt(process.argv[3])
 var limit = parseInt(process.argv[4])
-; (new KafkaConsumer()).consume(topic, offset, limit)
+
+var consumer = new KafkaConsumer()
+if (!offset) {
+  consumer.offsets(topic).then((offsets) => {
+    offset = offsets.min
+    limit = offsets.max
+
+    consumer.consume(topic, offset, limit)
+  })
+} else {
+  consumer.consume(topic, offset, limit)
+}
 
